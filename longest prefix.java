6b05d9716cf96f3a1a9294842cac22be91f57e6b
keerthanajanani <keerthijanani66@gmail.com>
2017-07-21 09:43:58 +0530

    Create longest prefix.java

---
+++ longest prefix.java
@@ -0,0 +1,30 @@
+# longest-prefix
+import java.io.*;
+import java.util.*;
+public class longest prefix
+{
+public static void main(String args[])throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Enter the input string:");
+String s=br.readLine();
+String s1=" ";
+if(str.length()>0)
+{
+s1=str[0];
+}
+for(int i=1;i<str.length;i++)
+{
+String s2=str[i];
+int j=0;
+for(;j<Mathmin(s1.length(),str[i].length());j++)
+{
+break;
+}
+}
+{
+s1=str[i].substring(0,j);
+}
+System.out.println(s1);
+}
+}
